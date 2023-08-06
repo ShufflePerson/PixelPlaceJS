3a089efb242f929ee551761344848d1fe50306d4
DarknessTom <121307743+DarknessTomDev@users.noreply.github.com>
2023-08-06 22:28:00 +0300

    Remove default code

--- src/index.ts
+++ src/index.ts
@@ -15,28 +15,3 @@ export { Utils };
 
 
 initLogger();
-
-if (require.main === module) {
-  (async () => {
-    if (!fs.existsSync("./temp")) fs.mkdirSync("./temp");
-    if (!fs.existsSync("./data")) fs.mkdirSync("./data");
-
-
-    let world = new World(7);
-    await world.Init();
-
-    let pp = new PixelPlace([""], world, 7);
-    await pp.Init();
-
-    await pp.render.drawRect({
-        x: 1276,
-        y: 1242
-    }, {
-        x: 100,
-        y: 100
-    }, 5) 
-    
-
-    console.log("PP is running");
-  })();
-}
