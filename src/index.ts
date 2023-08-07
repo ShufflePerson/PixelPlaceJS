31d7a41420db41e1522bb2a48a37b12e2a316328
DarknessTom <121307743+DarknessTomDev@users.noreply.github.com>
2023-08-07 13:06:51 +0300

    Removed unused imports

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,7 @@ import initLogger from "./Logging/initLogger";
 import EPackets from "./PixelPlace/Enums/EPackets";
 import PixelPlace from "./PixelPlace/PixelPlace";
 import World from "./PixelPlace/World/World";
-import fs from "fs";
 import Types from "./PixelPlace/Types/Types";
-import packPixel from "./PixelPlace/World/Utils/packPixel";
 import Utils from "./Utils";
 
 export { World };
