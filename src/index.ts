bb1d1387fca604774086c4d58a92a562f1984d78
DarknessTom <121307743+DarknessTomDev@users.noreply.github.com>
2023-08-07 18:28:00 +0300

    Export `Auth`

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,13 @@ import PixelPlace from "./PixelPlace/PixelPlace";
 import World from "./PixelPlace/World/World";
 import Types from "./PixelPlace/Types/Types";
 import Utils from "./Utils";
+import Auth from "./PixelPlace/Bot/Auth/Auth";
 
 export { World };
 export { PixelPlace };
 export { EPackets };
 export { Types };
 export { Utils };
+export { Auth };
 
 initLogger();
