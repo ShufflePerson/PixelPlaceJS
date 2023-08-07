97d71778ee0d1a7b3a79adcd8f4092fcd637ad1e
DarknessTom <121307743+DarknessTomDev@users.noreply.github.com>
2023-08-07 13:07:41 +0300

    Added `drawImage` function

--- src/PixelPlace/Render/Render.ts
+++ src/PixelPlace/Render/Render.ts
@@ -1,4 +1,5 @@
 import Bot from "../Bot/Bot";
+import { drawLinearImage } from "./Logic/Image/drawLinearImage";
 import drawRectLinear from "./Logic/Rect/drawLinearRect";
 import IVector2D from "./Types/IVector2D";
 
@@ -7,6 +8,10 @@ class Render {
 
   public async drawRect(position: IVector2D, size: IVector2D, color: number): Promise<void> {
     return drawRectLinear(position, size, color, this.bots);
+  }
+
+  public async drawImage(position: IVector2D, imagePath: string, size: number): Promise<void> {
+    return drawLinearImage(position, imagePath, size, this.bots);
   }
 }
 
