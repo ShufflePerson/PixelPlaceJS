5f2a0f379f97e861e9c338e98f93c370ac0994fa
DarknessTom <121307743+DarknessTomDev@users.noreply.github.com>
2023-08-07 13:47:16 +0300

    Formatting

--- src/Utils.ts
+++ src/Utils.ts
@@ -2,11 +2,10 @@ import getPxPColor from "./PixelPlace/Helpers/Render/getPxPColor";
 import { getPalive } from "./PixelPlace/Helpers/getPAlive";
 import fetchCanvasPNG from "./PixelPlace/World/Utils/fetchCanvasPNG";
 
-
 namespace Utils {
-    export let generatePingAlive = getPalive;
-    export let getColorFromRGB = getPxPColor;
-    export let getLatestCanvasPNG = fetchCanvasPNG;
+  export let generatePingAlive = getPalive;
+  export let getColorFromRGB = getPxPColor;
+  export let getLatestCanvasPNG = fetchCanvasPNG;
 }
 
 export default Utils;
