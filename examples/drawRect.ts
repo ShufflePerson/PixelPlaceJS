20f0ed3b0ec85588e1a208251971c83e4f27b9f0
DarknessTom <121307743+DarknessTomDev@users.noreply.github.com>
2023-08-07 18:30:07 +0300

    Updated to new `Auth` system

--- examples/drawRect.ts
+++ examples/drawRect.ts
@@ -1,10 +1,19 @@
-import { PixelPlace, World, Types, EPackets } from '../src'
+import { PixelPlace, World, Types, EPackets, Auth } from '../src'
 
 
 
 async function main() {
+
+    let accounts: Auth[] = [
+        new Auth(null, {
+            authId: "",
+            authKey: "",
+            authToken: ""
+        })
+    ]
+
     let world = new World(7);
-    let pixelplace = new PixelPlace(["Account@gmail.com"], world, 7);
+    let pixelplace = new PixelPlace(accounts, world, 7);
 
     await world.Init();
     await pixelplace.Init();
