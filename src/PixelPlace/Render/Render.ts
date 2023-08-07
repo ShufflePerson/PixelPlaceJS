d3f8d8aaf86cbde4815b085db40af2f0139150de
DarknessTom <121307743+DarknessTomDev@users.noreply.github.com>
2023-08-07 16:42:06 +0300

    Updated `drawLinearImage` parameters

--- src/PixelPlace/Render/Render.ts
+++ src/PixelPlace/Render/Render.ts
@@ -1,17 +1,19 @@
 import Bot from "../Bot/Bot";
+import PixelPlace from "../PixelPlace";
 import { drawLinearImage } from "./Logic/Image/drawLinearImage";
 import drawRectLinear from "./Logic/Rect/drawLinearRect";
+import IImageData from "./Types/IImageData";
 import IVector2D from "./Types/IVector2D";
 
 class Render {
-  constructor(private bots: Bot[]) {}
+  constructor(private pixelplace: PixelPlace) {}
 
   public async drawRect(position: IVector2D, size: IVector2D, color: number): Promise<void> {
-    return drawRectLinear(position, size, color, this.bots);
+    return drawRectLinear(position, size, color, this.pixelplace.bots);
   }
 
-  public async drawImage(position: IVector2D, imagePath: string, size: number): Promise<void> {
-    return drawLinearImage(position, imagePath, size, this.bots);
+  public async drawImage(position: IVector2D, imagePath: string, size: number, protect: boolean = false): Promise<IImageData> {
+    return drawLinearImage(position, imagePath, size, protect, this.pixelplace);
   }
 }
 
