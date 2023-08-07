d3de4f9d77117ba25e07efc655bcc4c24058f7bd
DarknessTom <121307743+DarknessTomDev@users.noreply.github.com>
2023-08-07 13:06:58 +0300

    Added `dev` parameter

--- src/Logging/initLogger.ts
+++ src/Logging/initLogger.ts
@@ -1,7 +1,8 @@
+import isDev from "./isDev";
 import registerTransports from "./registerTransports";
 
-function initLogger() {
-  registerTransports();
+function initLogger(dev: boolean = isDev()) {
+  registerTransports(dev);
 }
 
 export default initLogger;
