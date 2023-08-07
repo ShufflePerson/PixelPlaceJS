0928795d1de64757a5b2f952bb0e2245227ec2f6
DarknessTom <121307743+DarknessTomDev@users.noreply.github.com>
2023-08-07 16:40:31 +0300

    Implemented `isInsidePoint` function

---
+++ src/PixelPlace/Helpers/Math/isInsidePoint.ts
@@ -0,0 +1,6 @@
+import IVector2D from "../../Render/Types/IVector2D";
+
+export default ((startPosition: IVector2D, size: IVector2D, target: IVector2D) => {
+    return startPosition.x <= target.x && target.x <= startPosition.x + size.x &&
+    startPosition.y <= target.y && target.y <= startPosition.y + size.y;
+})
