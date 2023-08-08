874f7a74a56b547f8b8592ea8832063470e9407c
DarknessTom <121307743+DarknessTomDev@users.noreply.github.com>
2023-08-09 00:51:11 +0300

    Refactored `Render` functions

--- src/PixelPlace/Render/Render.ts
+++ src/PixelPlace/Render/Render.ts
@@ -5,24 +5,17 @@ import unpackPixel from "../World/Utils/unPackPixel";
 import { EDrawingMode } from "./Enums/EDrawingMode";
 import { convertAndGetImage } from "./Logic/Image/convertAndGetImage";
 import drawBasic from "./Logic/Methods/drawBasic";
-import drawRectLinear from "./Logic/Rect/drawLinearRect";
+import getRect from "./Logic/Rect/getRect";
 import IImageData from "./Types/IImageData";
 import IVector2D from "./Types/IVector2D";
+import packPixel from "../World/Utils/packPixel";
+import getPackedPixel from "../World/Utils/getPackedPixel";
 
 class Render {
   constructor(private pixelplace: PixelPlace) { }
 
-  public async drawRect(position: IVector2D, size: IVector2D, color: number): Promise<void> {
-    return drawRectLinear(position, size, color, this.pixelplace.bots);
-  }
-
-  public async drawImage(position: IVector2D, imagePath: string, size: number, protect: boolean = false, mode: EDrawingMode = EDrawingMode.BASIC): Promise<IImageData> {
-    let imageData = await convertAndGetImage(position, imagePath, size, protect, this.pixelplace);
+  private async draw(imageData: IImageData, position: IVector2D, mode: EDrawingMode) {
     let { width, height } = imageData.metadata;
-
-    if (protect) {
-      this.pixelplace.RegisterProtectionZone(position.x, position.y, imageData);
-    }
 
     switch (mode) {
       case EDrawingMode.BASIC:
@@ -33,6 +26,37 @@ class Render {
         winston.log("error", "Given Drawing Mode is not a valid option. ", "Render", mode);
         break;
     }
+  }
+
+  public async drawRect(position: IVector2D, size: IVector2D, color: number, protect: boolean = false, mode: EDrawingMode = EDrawingMode.BASIC): Promise<IImageData> {
+    let buffer = getRect(position, size, color);
+    let imageData = {
+      buffer,
+      metadata: {
+        width: size.x,
+        height: size.y
+      }
+    }
+
+    if (protect) {
+      this.pixelplace.RegisterProtectionZone(position.x, position.y, imageData);
+    }
+
+
+    await this.draw(imageData, position, mode);
+
+
+    return imageData;
+  }
+
+  public async drawImage(position: IVector2D, imagePath: string, size: number, protect: boolean = false, mode: EDrawingMode = EDrawingMode.BASIC): Promise<IImageData> {
+    let imageData = await convertAndGetImage(imagePath, size);
+    
+    if (protect) {
+      this.pixelplace.RegisterProtectionZone(position.x, position.y, imageData);
+    }
+
+    await this.draw(imageData, position, mode);
 
     return imageData;
   }
