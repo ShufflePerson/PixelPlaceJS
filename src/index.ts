20056d9bd01e32d0e448074567104bb6cbeb9f4b
DarknessTom <121307743+DarknessTomDev@users.noreply.github.com>
2023-08-08 12:55:22 +0300

    Exposed `Bot`

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import dotenv from 'dotenv'
+dotenv.config()
 import initLogger from "./Logging/initLogger";
 import EPackets from "./PixelPlace/Enums/EPackets";
 import PixelPlace from "./PixelPlace/PixelPlace";
@@ -5,6 +7,7 @@ import World from "./PixelPlace/World/World";
 import Types from "./PixelPlace/Types/Types";
 import Utils from "./Utils";
 import Auth from "./PixelPlace/Bot/Auth/Auth";
+import Bot from "./PixelPlace/Bot/Bot";
 
 export { World };
 export { PixelPlace };
@@ -12,5 +15,6 @@ export { EPackets };
 export { Types };
 export { Utils };
 export { Auth };
+export { Bot }
 
 initLogger();
