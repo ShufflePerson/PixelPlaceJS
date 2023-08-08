a3b2e71f5c0683acbdbc2cf97c09fbf1ad72e49a
DarknessTom <121307743+DarknessTomDev@users.noreply.github.com>
2023-08-09 00:53:11 +0300

    Added `getPackedPixel`

--- src/PixelPlace/World/Utils/packPixel.ts
+++ src/PixelPlace/World/Utils/packPixel.ts
@@ -1,6 +1,8 @@
+import getPackedPixel from "./getPackedPixel";
+
 function packPixel(buffer: Buffer, offset: number, x: number, y: number, color: number) {
   try {
-    buffer.writeInt32BE((x << 20) | (y << 8) | color, offset);
+    buffer.writeInt32BE(getPackedPixel(x, y, color), offset);
   } catch (ex) {
     console.log(ex);
   }
