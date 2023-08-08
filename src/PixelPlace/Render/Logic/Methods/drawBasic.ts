90f386830318f37ed1db0b1e5a4250acbf5ec89e
DarknessTom <121307743+DarknessTomDev@users.noreply.github.com>
2023-08-08 13:05:31 +0300

    Implemented `drawBasic`

---
+++ src/PixelPlace/Render/Logic/Methods/drawBasic.ts
@@ -0,0 +1,14 @@
+import PixelPlace from "../../../PixelPlace";
+import unpackPixel from "../../../World/Utils/unPackPixel";
+import IVector2D from "../../Types/IVector2D";
+
+
+export default(async (position: IVector2D, width: number, height: number, buffer: Buffer, pixelplace: PixelPlace): Promise<void> => {
+    for (let h = 0; h < height; h++) {
+        for (let w = 0; w < width; w++) {
+          let pixel = unpackPixel(buffer, (h * width + w) * 4);
+          let [x, y, color] = pixel;
+          await pixelplace.placePixel(position.x + x, position.y + y, color);
+        }
+    }
+})
