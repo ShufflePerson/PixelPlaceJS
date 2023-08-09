17e26fa5a75c4be7896cb27064af90f281dbb9a7
DarknessTom <121307743+DarknessTomDev@users.noreply.github.com>
2023-08-09 17:43:18 +0300

    Updated example to use `cache.json`

--- examples/drawImage.ts
+++ examples/drawImage.ts
@@ -1,16 +1,43 @@
-import { PixelPlace, World, Auth } from '../src/index'
+import { PixelPlace, World, Auth, EDrawingMode } from '../src/index'
+import fs from 'fs';
 
+function loadCache() {
+    if (!fs.existsSync("./cache.json"))
+    throw new Error("No cache.json found. Please create one");
 
+    return JSON.parse(fs.readFileSync("./cache.json").toString("utf-8"))
+}
+
+function loadAuths(): Auth[] {
+    let cache = loadCache();
+    let auths: Auth[] = [];
+    
+    for (let data of cache) {
+        let { authId, authToken, authKey } = data;
+        if (authId && authToken && authKey) {
+            auths.push(new Auth(null, data));
+        } else {
+            console.warn(data, "Is invalid session data.");
+        }
+    }
+
+    return auths;
+}
+
+function saveNewTokens(auths: Auth[]) {
+    let cache: Array<any> = [];
+    for (let auth of auths) {
+        if (auth.didTokensRotate) {
+            cache.push(auth.getSessionData());
+        }
+    }
+
+    fs.writeFileSync("./cache.json", JSON.stringify(cache));
+}
 
 async function main() {
 
-    let accounts: Auth[] = [
-        new Auth(null, {
-            authId: "", //Fill these values
-            authKey: "",
-            authToken: ""
-        })
-    ]
+    let accounts = loadAuths();
 
     let world = new World(7, accounts[0]);
     let pixelplace = new PixelPlace(accounts, world, 7);
@@ -18,9 +45,12 @@ async function main() {
     await world.Init();
     await pixelplace.Init();
 
-    let [x, y] = [1336, 1839];
+    saveNewTokens(accounts);
 
-    let imageData = await pixelplace.render.drawImage({x, y}, "test.png", 200, true);
+    let [x, y] = [1286, 1823];
+    
+    await pixelplace.render.drawImage({x, y}, "test.png", 0, true);
+    //await pixelplace.render.drawRect({x, y}, {x: 100, y: 100}, Math.round(Math.random() * 32), false);
 }
 
 main();
