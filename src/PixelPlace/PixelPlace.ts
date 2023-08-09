862d9c563be46663dd1d5af12593428ac5b11bf8
DarknessTom <121307743+DarknessTomDev@users.noreply.github.com>
2023-08-09 20:16:18 +0300

    New system for `placePixel` and `protect`

--- src/PixelPlace/PixelPlace.ts
+++ src/PixelPlace/PixelPlace.ts
@@ -8,13 +8,23 @@ import EPackets from "./Enums/EPackets";
 import isInsidePoint from "./Helpers/Math/isInsidePoint";
 import unpackPixel from "./World/Utils/unPackPixel";
 import IVector2D from "./Render/Types/IVector2D";
+import sleep from "../Utils/sleep";
+
+const eventLoopQueue = () => {
+  return new Promise((resolve, reject) => 
+    setImmediate(() => {
+      resolve(null);
+    })
+  );
+}
 
 class PixelPlace {
   public bots: Bot[] = [];
   public render: Render;
+  private toProtect: number[][] = [];
   private protectedZones: Array<{
-    position: IVector2D,
-    imageData: IImageData
+    position: IVector2D;
+    imageData: IImageData;
   }> = [];
 
   constructor(
@@ -38,33 +48,47 @@ class PixelPlace {
     this.render = new Render(this);
   }
 
-  public registerProtectionZone(startX: number, startY: number, original: IImageData) {
+  private getProtectId(startX: number, startY: number, original: IImageData): number {
+    return Math.round((startX * startY) / original.metadata.height + original.metadata.width);
+  }
+
+  public registerProtectionZone(startX: number, startY: number, original: IImageData): number {
     this.protectedZones.push({
       position: {
         x: startX,
         y: startY
       },
       imageData: original
-    })
-  
+    });
+
+    return this.getProtectId(startX, startY, original);
   }
 
-  
+  public removeProtection(id: number) {
+    for (let i = 0; i < this.protectedZones.length; i++) {
+      let zone = this.protectedZones[i];
+      let zoneId = this.getProtectId(zone.position.x, zone.position.y, zone.imageData);
+      if (id == zoneId) {
+        this.protectedZones.splice(i, 1);
+        break;
+      }
+    }
+  }
 
   public async placePixel(x: number, y: number, color: number, force: boolean = false): Promise<Boolean> {
-    return new Promise((resolve, reject) => {
-      let interv = setInterval(() => {
+    return new Promise(async (resolve, reject) => {
+      let placed = false;
+      while (!placed) {
         for (let bot of this.bots) {
-          if(bot.placePixel(x, y, color, force)) {
+          placed = bot.placePixel(x, y, color, force);
+          if (placed) {
             resolve(true);
-            clearInterval(interv);
             break;
-            return;
           }
         }
-      }, 1)
-    })
-
+        await eventLoopQueue();
+      }
+    });
   }
 
   public async Init(): Promise<void> {
@@ -92,14 +116,22 @@ class PixelPlace {
           let [x, y, color] = pixel;
           if (isInsidePoint({ x: startX, y: startY }, { x: width - 1, y: height - 1 }, { x, y })) {
             let originalPixel = unpackPixel(protectedZone.imageData.buffer, ((y - startY) * width + (x - startX)) * 4);
-            if (color != originalPixel[2])
-              this.placePixel(x, y, originalPixel[2], true);
+            if (color != originalPixel[2]) {
+              this.toProtect.push([x, y, originalPixel[2]]);
+            }
           }
         }
       }
     });
 
-
+    setInterval(async () => {
+      for (let i = 0; i < this.toProtect.length; i++) {
+        let pixel = this.toProtect[i];
+        let [x, y, color] = pixel;
+        this.toProtect.splice(i, 1);
+        this.placePixel(x, y, color, true);
+      }
+    }, 20);
 
     winston.log("info", "Bots are ready.", "PixelPlace", this.bots.length);
   }
