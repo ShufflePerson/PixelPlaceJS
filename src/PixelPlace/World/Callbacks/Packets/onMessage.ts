e17ef7743c6d660c92a5a550d9c122f2f70e9f95
DarknessTom <121307743+DarknessTomDev@users.noreply.github.com>
2023-08-12 17:04:34 +0300

    Edit logging

--- src/PixelPlace/World/Callbacks/Packets/onMessage.ts
+++ src/PixelPlace/World/Callbacks/Packets/onMessage.ts
@@ -6,7 +6,7 @@ let winston = getWorldLogger();
 function onMessage(chatMessage: IChatMessage) {
   winston.log("info", "Chat Message", "World", chatMessage.username, chatMessage.message);
   if (chatMessage.admin || chatMessage.chatmod || chatMessage.mod) {
-    winston.log("info", "Mod has sent a chat message.", chatMessage.username);
+    winston.log("info", "Mod has sent a chat message.", "onMessage", chatMessage.username);
   }
 }
 
