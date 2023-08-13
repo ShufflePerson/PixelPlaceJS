89e8087e64735946f38b6c48616e75bd106e2573
DarknessTom <121307743+DarknessTomDev@users.noreply.github.com>
2023-08-13 15:35:38 +0300

    Added `Buffer` namespace for `writeIntoBuffer` and `readFromBuffer`

--- src/Utils.ts
+++ src/Utils.ts
@@ -1,11 +1,25 @@
 import getPxPColor from "./PixelPlace/Helpers/Render/getPxPColor";
 import { getPalive } from "./PixelPlace/Helpers/getPAlive";
 import fetchCanvasPNG from "./PixelPlace/World/Utils/fetchCanvasPNG";
+import packPixel from "./PixelPlace/World/Utils/packPixel";
+import unpackPixel from "./PixelPlace/World/Utils/unPackPixel";
 
 namespace Utils {
   export let generatePingAlive = getPalive;
   export let getColorFromRGB = getPxPColor;
   export let getLatestCanvasPNG = fetchCanvasPNG;
+  
+  export namespace Buffer {
+    export function writeIntoBuffer(x: number, y: number, width: number, color: number, buffer: Buffer) {
+      packPixel(buffer, (y * width + x) * 4, x, y, color);
+    }
+  
+    export function readFromBuffer(x: number, y: number, width: number, buffer: Buffer) {
+        return unpackPixel(buffer, (y * width + x) * 4);
+    }
+  }
+
+
 }
 
 export default Utils;
